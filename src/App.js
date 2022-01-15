5e33f418496e482ea2fd7af5ff4e32a9b05ccbd6
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-01-15 20:30:20 +0530

    create keypair for BaseAccount Create Keypair for BaseAccount

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import idl from './idl.json';
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
+import kp from './keypair.json'
 
 // SystemProgram is a reference to the Solana runtime!
 const { SystemProgram, Keypair } = web3;
 
 // Create a keypair for the account that will hold the GIF data.
-let baseAccount = Keypair.generate();
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
 
 // Get our program's id from the IDL file.
 const programID = new PublicKey(idl.metadata.address);
@@ -95,6 +98,7 @@ const App = () => {
         signers: [baseAccount]
       });
       console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      await getCreatorList();
     } catch(error) {
       console.log("Error creating BaseAccount account:", error)
     }
